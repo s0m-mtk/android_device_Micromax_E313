d79c66e137424116ab0152cdcfa26d392ee158bf
tandob-mtk <meetsoumen.das1@gmail.com>
2016-08-25 22:40:52 +0530

    bring up marshmallow

---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/device_E313.mk
---
+++ BoardConfig.mk
@@ -0,0 +1,154 @@
+# inherit from the proprietary version
+-include vendor/Micromax/E313/BoardConfigVendor.mk
+
+# GPS
+TARGET_SPECIFIC_HEADER_PATH := device/Micromax/E313/include
+
+# Board
+TARGET_BOARD_PLATFORM := mt6592
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+ARCH_ARM_HAVE_VFP := true
+TARGET_CPU_SMP := true
+TARGET_ARCH := arm
+ARCH_ARM_HAVE_NEON := true
+TARGET_NO_BOOTLOADER := true
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a7
+TARGET_CPU_VARIANT:= cortex-a7
+TARGET_CPU_MEMCPY_OPT_DISABLE := true
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+# Enable dex-preoptimization
+WITH_DEXPREOPT := false
+DONT_DEXPREOPT_PREBUILTS := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := mt6592
+
+# Partitions & Image
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16485760  # 0x00a00000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16485760  # 0x00a00000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1577058304  # 0x5e000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1237319680  # 0x49c00000
+BOARD_CACHEIMAGE_PARTITION_SIZE := 134217728  # 0x08000000
+BOARD_CACHEIMAGE_TYPE := ext4
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# Flags
+TARGET_GLOBAL_CFLAGS   += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+# Kernel 
+BOARD_KERNEL_CMDLINE :=
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+TARGET_PREBUILT_KERNEL := device/Micromax/E313/kernel
+BOARD_CUSTOM_BOOTIMG_MK := device/Micromax/E313/bootimg.mk
+BOARD_CUSTOM_BOOTIMG := true
+TARGET_KMODULES := true
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := E313,lcsh92_cwet_kk
+
+COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+
+# Disable memcpy opt (for audio libraries)
+TARGET_CPU_MEMCPY_OPT_DISABLE := true
+
+# EGL settings
+BOARD_EGL_CFG := device/Micromax/E313/configs/egl.cfg
+USE_OPENGL_RENDERER := true
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+BOARD_EGL_NEEDS_HANDLE_VALUE := true
+BOARD_EGL_NEEDS_FNW := true
+TARGET_REQUIRES_SYNCHRONOUS_SETSURFACE := true
+
+# MTK HARDWARE
+BOARD_HAS_MTK_HARDWARE := true
+MTK_HARDWARE := true
+BOARD_USES_LEGACY_MTK_AV_BLOB := true
+COMMON_GLOBAL_CFLAGS += -DMTK_HARDWARE -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL
+COMMON_GLOBAL_CPPFLAGS += -DMTK_HARDWARE
+BLOCK_BASED_OTA := false
+
+# Healthd & Charging
+HEALTHD_ENABLE_TRICOLOR_LED := true
+RED_LED_PATH := /sys/class/leds/red/brightness
+GREEN_LED_PATH := /sys/class/leds/green/brightness
+BLUE_LED_PATH := /sys/class/leds/blue/brightness
+# Next lines, fix charging-mod in power off. It needs to modify the init.cpp, too.
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/BOOT/BOOT/boot/boot_mode
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.mtk
+BOARD_HEALTHD_CUSTOM_CHARGER_RES := device/Micromax/E313/charger/images
+
+# RIL
+BOARD_RIL_CLASS := ../../../device/Micromax/E313/ril/
+
+BOARD_CONNECTIVITY_VENDOR := MediaTek
+BOARD_CONNECTIVITY_MODULE := conn_soc
+
+# WIFI
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_mt66xx
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_mt66xx
+WIFI_DRIVER_FW_PATH_PARAM:="/dev/wmtWifi"
+WIFI_DRIVER_FW_PATH_STA:=STA
+WIFI_DRIVER_FW_PATH_AP:=AP
+WIFI_DRIVER_FW_PATH_P2P:=P2P
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_MTK := true
+BOARD_BLUETOOTH_DOES_NOT_USE_RFKILL := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/Micromax/E313/bluetooth
+
+# Sensors
+TARGET_NO_SENSOR_PERMISSION_CHECK := true
+
+# CWM
+TARGET_RECOVERY_FSTAB := device/Micromax/E313/rootdir/recovery.fstab
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+# TWRP
+#RECOVERY_VARIANT=twrp
+DEVICE_RESOLUTION := 720x1280
+BOARD_HAS_LARGE_FILESYSTEM := true
+TW_NO_USB_STORAGE := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
+TW_INTERNAL_STORAGE_PATH := "/emmc"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "emmc"
+TW_EXTERNAL_STORAGE_PATH := "/sdcard"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "sdcard"
+
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/virtual/android_usb/android0/f_mass_storage/lun%d/file"
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+# Selinux
+BOARD_SEPOLICY_DIRS += device/Micromax/E313/sepolicy
+
+# Hack for building without kernel sources
+$(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
+
+# Use old sepolicy version
+POLICYVERS := 26
+
+# Release tools
+TARGET_RELEASETOOLS_EXTENSIONS := device/Micromax/E313/releasetools
+
+# Include Open GApps
+INCLUDE_OPEN_GAPPS := true
